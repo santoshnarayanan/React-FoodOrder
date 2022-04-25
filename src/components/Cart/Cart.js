04395bdbb687c35e8c41c87e38ccec9d83d98a94
santosh33 <santosh33@gmail.com>
2022-04-25 22:51:26 +0530

    Creating Modal Component to display Total Amount

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
 
@@ -6,7 +7,7 @@ const Cart = (props) => {
         {id:'c1', name:'Sushi', amount:2, price:12.99}]
         .map(item=><li>{item.name}</li>)}</ul>;
     return (
-        <div>
+        <Modal>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
@@ -16,7 +17,7 @@ const Cart = (props) => {
                 <button className={classes['button--alt']}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     );
 };
 
