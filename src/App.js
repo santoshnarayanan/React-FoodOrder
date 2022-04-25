1df3029e1b4d09fa177da50ee23cbcb60b35bb0f
santosh33 <santosh33@gmail.com>
2022-04-26 01:04:04 +0530

    Using Context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, {Fragment, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
-
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
@@ -16,13 +16,13 @@ function App() {
   };
 
   return (
-    <Fragment>
-        {cartIsShown && < Cart onClose={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
-        <main>
-            <Meals />
-        </main>
-    </Fragment>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
